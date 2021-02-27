a8df9e9672650fb9c2dea151f2423a0cba116432
Katip <Katip@DESKTOP-6P2FP43>
2021-02-27 16:15:53 +0300

    logic update.

--- Business/Constants/Messages.cs
+++ Business/Constants/Messages.cs
@@ -11,5 +11,8 @@ namespace Business.Constants
         public static string ProductNameInvalid = "ürün ismi geçersiz";
         public static string MaintenanceTime="Sistem bakımda";
         public static string ProductListed= "Ürünler listelendi";
+        public static string ProductCountOfCategoryError = "bir kategoride en fazla 10 ürün olabilir";
+        public static string ProductNameAlreadyExists = "Bu isimde zaten başka kategori var";
+        public static string CategoryLimitExceded = "kategori limiti aşıldığı için yeni ürün eklenemiyor";
     }
 }
